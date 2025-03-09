ab86deb04406f4e5281353a7b0fb17403960c637
durai145 <durai145@live.in>
2025-03-09 07:06:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain.llms import OpenAI
-st.set_page_config(page_title="🦜🔗 Quickstart App")
-st.title('🦜🔗 Quickstart App')
+st.set_page_config(page_title="FoxMind")
+st.title('fox mind')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
